21e2befac268fa11e4f2b0eb5839547ee6123f17
Bitcube <bitcube@Bitcubes-iMac.local>
2019-09-17 11:08:25 +0200

    added links to stylesheets

--- app/public/wp-content/themes/temporall_site/functions.php
+++ app/public/wp-content/themes/temporall_site/functions.php
@@ -10,7 +10,9 @@ function theme_styles()
     wp_enqueue_style( 'index', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'top-nav', get_template_directory_uri() . '/static-components/css/top-nav.css' );
     wp_enqueue_style( 'footer', get_template_directory_uri() . '/static-components/css/footer.css' );
-
+	wp_enqueue_style( 'side-bar', get_template_directory_uri() . '/static-components/css/side-banner-contact-buttons.css' );
+	wp_enqueue_style( 'hero-image', get_template_directory_uri() . '/static-components/css/hero-image-left-text-and-btn.css' );
+	wp_enqueue_style( 'various-logos', get_template_directory_uri() . '/static-components/css/various-logos-section.css' );
 
 	// // Conditionally load the FlexSlider CSS on the homepage
 	// if(is_page('home')) {
