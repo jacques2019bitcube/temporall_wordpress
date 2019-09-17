340eb3fc26a3c41885fad66bb577cd3f936e7b7d
Bitcube <bitcube@Bitcubes-iMac.local>
2019-09-17 11:07:57 +0200

    removed code and made their own componets

--- app/public/wp-content/themes/temporall_site/index.php
+++ app/public/wp-content/themes/temporall_site/index.php
@@ -2,232 +2,154 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 
 <html>
-<?php wp_head() ?>
+
 <head>
-
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- <script>
-        $(function () {
-            $('#top-nav').load('./static-components/html/top-nav.html');
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        });
-    </script> -->
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="./static-components/css/footer.css">
-
-    <link rel="stylesheet" href="./static-components/css/top-nav.css">
-    <link rel="stylesheet" href="./static-components/css/side-banner-contact-buttons.css">
-    <link rel="stylesheet" href="./static-components/css/integration-card.css">
-    <link rel="stylesheet" href="./static-components/css/left-text-right-image-card.css">
-    
     <title>title</title>
-    
+    <?php wp_head() ?>
 </head>
 
 <body>
-    <div class="container-fluid">
 
-        <!-- <div id="new"></div> -->
+    <?php get_header(); ?>
+    <?php include "wp-content/themes/temporall_site/static-components/html/side-banner-contact-buttons.html" ?>
 
-        <?php include "wp-content/themes/temporall_site/static-components/html/top-nav.html" ?>
-        
-        
-        <div w3-include-html="./static-components/html/side-banner-contact-buttons.html"></div>
-
-        <!-- <div id="top-nav" w3-include-html="./static-components/html/top-nav.html"></div> -->
-
-        <div id="top-nav"></div>
-
-        <div class="container-fluid">
-            <div class="hero-text-and-image">
-                <div class="hero-text">
-                    <p class="hero-text-title">
-                        Workbench
-                    </p>
-                    <p class="hero-text-body">
-                        <span style="color:#ED7401;">Organisational</span> insights that <br> matter the most
-                    </p>
-                    <button class="hero-button">
-                        Contact us
-                    </button>
-                    <a class="hero-link" href="#">Watch the video ></a>
-                </div>
-
-                <div class="hero-image">
-                    <img class="img-responsive" style="height: 22.5%;width: 1000px;"
-                        src="wp-content/themes/temporall_site/assets/images/Macbook-Mockup-V1-cut.png">
-                </div>
-
-            </div>
-        </div>
-
-        <div class="container-fluid">
-            <div class="why-us">
-                <div class="why-us-text">
-                    Changing the way you think about your people, culture and organisation
-                </div>
-                <div class="various-logos">
-                    <div class="logo-inner-container">
-
-                        <div style="display: inline-block">
-                            <img src="wp-content/themes/temporall_site/assets/images/Google.png">
-                        </div>
-                        <div style="display: inline-block">
-                            <img src="wp-content/themes/temporall_site/assets/images/Dyson.png">
-                        </div>
-                        <div style="display: inline-block">
-
-                            <img src="wp-content/themes/temporall_site/assets/images/Rubekon.png">
-                        </div>
-                        <div style="display: inline-block">
-                            <img src="wp-content/themes/temporall_site/assets/images/Arup.png">
-                        </div>
-                        <div style="display: inline-block">
-
-                            <img c src="wp-content/themes/temporall_site/assets/images/EFQM.png">
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+    <?php include "wp-content/themes/temporall_site/static-components/html/hero-image-left-text-and-btn.html" ?>
 
 
-        <div class="product-statement">
-            <div class="productStatement-wrapper">
-                <div class="product-statement-small-img-and-text-text-left">
-                    <div class="product-statement-small-img">
-                        <img src="wp-content/themes/temporall_site/assets/images/pie_chart (1).svg">
-                    </div>
-                    <div class="product-statment-text">
-                        <p>Intelligent insights, <br> informed decision making</p>
-                        <p>Powerful data analytics, machine learning, and <br> dashboards
-                            transforms quantitative, qualitative, sentiment <br> and network analysis data from
-                            people,
-                            teams and <br> systems into high impact insights.</p>
-                    </div>
-                </div>
-                <div class="product-statement-main-img">
-                    <img class="img-responsive" src="wp-content/themes/temporall_site/assets/images/Rectangle 532.png">
-                </div>
-            </div>
 
-        </div>
+    <div class="why-us-text heading3">
+        Changing the way you think about your people, culture and organisation
+    </div>
 
 
+    <?php include "wp-content/themes/temporall_site/static-components/html/various-logos-section.html" ?>
+   
+
+    <div class="product-statement">
         <div class="productStatement-wrapper">
-            <div class="product-statement-main-img">
-                <img class="img-responsive" src="wp-content/themes/temporall_site/assets/images/Temporall-Macbook-Screen-4.png">
-            </div>
-            <div class="product-statement-small-img-and-text-text-right">
+            <div class="product-statement-small-img-and-text-text-left">
                 <div class="product-statement-small-img">
-                    <img src="wp-content/themes/temporall_site/assets/images/score (1).svg">
+                    <img src="wp-content/themes/temporall_site/assets/images/pie_chart (1).svg">
                 </div>
                 <div class="product-statment-text">
-                    <p>Structured frameworks, <br> real-time data capture</p>
-                    <p>Use our industry-leading Organisational and Manager <br> Indexes or
-                        Partner Indexes for rapid diagnosis of <br> strengths and shortcomings via our App, Slack,
-                        <br> Workplace by Facebook, Teams and email to capture <br> continuous data at scale.
+                    <p class="heading3">Intelligent insights, <br> informed decision making</p>
+                    <p class="body-text">Powerful data analytics, machine learning, and <br> dashboards
+                        transforms quantitative, qualitative, sentiment <br> and network analysis data from
+                        people,
+                        teams and <br> systems into high impact insights.</p>
                 </div>
             </div>
-        </div>
-
-        <div class="integration-section">
-            <div class="integration-logos">
-
-                <div class="integration-items integration-text-section">
-                    <div class="integration-text">
-                        <p> Meet people <br> where they work </p>
-                        <p>Integrate with your favourite systems
-                            <br> and gather data in one place</p>
-                    </div>
-                </div>
-                <div class="integration-items integration-logo-section">
-                    <div class="integration-items integraion-logo-spacing">
-                        <img style="width: 79px; height: 9.875%;"
-                            src="wp-content/themes/temporall_site/assets/images/Temporall-App-Icon-rounded-800x800.png">
-                    </div>
-                    <div class="integration-items integraion-logo-spacing">
-                        <img style="width: 177px; height:52%;" src="wp-content/themes/temporall_site/assets/images/slack-2@2x.png">
-                    </div>
-                    <div class="integration-items integraion-logo-spacing">
-                        <img style="width:122px;  height:35.88%;"
-                            src="wp-content/themes/temporall_site/assets/images/Workplace_Partner-Logo_Two-Line_Grey-Box_CMYK.png">
-                    </div>
-                    <div class="integration-items integraion-logo-spacing">
-                        <img style="width:122px;  height:30.5%;" src="wp-content/themes/temporall_site/assets/images/gcp-partner-0-1_@2x-400x195.png">
-                    </div>
-                    <div class="integration-items integraion-logo-spacing">
-                        <img style="width: 79px; height:19.75%;" src="wp-content/themes/temporall_site/assets/images/microsoft-teams-400x396.png">
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-
-        <div class="productStatement-wrapper">
             <div class="product-statement-main-img">
-                <img src="wp-content/themes/temporall_site/assets/images/Temporall-Macbook-Screen-2.png">
+                <img class="img-responsive" src="wp-content/themes/temporall_site/assets/images/Rectangle 532.png">
             </div>
-            <div class="product-statement-small-img-and-text-text-right">
+        </div>
+
+    </div>
+
+
+    <div class="productStatement-wrapper">
+        <div class="product-statement-main-img">
+            <img class="img-responsive" src="wp-content/themes/temporall_site/assets/images/Temporall-Macbook-Screen-4.png">
+        </div>
+        <div class="product-statement-small-img-and-text-text-right">
+            <div class="product-statement-small-img">
+                <img src="wp-content/themes/temporall_site/assets/images/score (1).svg">
+            </div>
+            <div class="product-statment-text">
+                <p class="heading3">Structured frameworks, <br> real-time data capture</p>
+                <p class="body-text">Use our industry-leading Organisational and Manager <br> Indexes or
+                    Partner Indexes for rapid diagnosis of <br> strengths and shortcomings via our App, Slack,
+                    <br> Workplace by Facebook, Teams and email to capture <br> continuous data at scale.
+            </div>
+        </div>
+    </div>
+
+    <div class="integration-section">
+        <div class="integration-logos">
+
+            <div class="integration-items integration-text-section">
+                <div class="integration-text">
+                    <p class="heading3"> Meet people <br> where they work </p>
+                    <p class="body-text">Integrate with your favourite systems
+                        <br> and gather data in one place</p>
+                </div>
+            </div>
+            <div class="integration-items integration-logo-section">
+                <div class="integration-items integraion-logo-spacing">
+                    <img style="width: 79px; height: 9.875%;" src="wp-content/themes/temporall_site/assets/images/Temporall-App-Icon-rounded-800x800.png">
+                </div>
+                <div class="integration-items integraion-logo-spacing">
+                    <img style="width: 177px; height:52%;" src="wp-content/themes/temporall_site/assets/images/slack-2@2x.png">
+                </div>
+                <div class="integration-items integraion-logo-spacing">
+                    <img style="width:122px;  height:35.88%;" src="wp-content/themes/temporall_site/assets/images/Workplace_Partner-Logo_Two-Line_Grey-Box_CMYK.png">
+                </div>
+                <div class="integration-items integraion-logo-spacing">
+                    <img style="width:122px;  height:30.5%;" src="wp-content/themes/temporall_site/assets/images/gcp-partner-0-1_@2x-400x195.png">
+                </div>
+                <div class="integration-items integraion-logo-spacing">
+                    <img style="width: 79px; height:19.75%;" src="wp-content/themes/temporall_site/assets/images/microsoft-teams-400x396.png">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="productStatement-wrapper">
+        <div class="product-statement-main-img">
+            <img src="wp-content/themes/temporall_site/assets/images/Temporall-Macbook-Screen-2.png">
+        </div>
+        <div class="product-statement-small-img-and-text-text-right">
+            <div class="product-statement-small-img">
+                <img class="img-responsive" src="wp-content/themes/temporall_site/assets/images/ic_lock_24px (1).svg">
+            </div>
+            <div class="product-statment-text">
+                <p class="heading3">Discover and unlock <br> previously untapped value
+                    <p class="body-text">Combine data from Workbench and enterprise <br> productivity
+                        tools, CRM, and HR systems to discover and <br> unlock opportunities to deliver
+                        cross-systems insight; <br> build networks, improve productivity, drive engagement.
+            </div>
+        </div>
+    </div>
+
+
+    <div class="product-statement">
+        <div class="productStatement-wrapper">
+            <div class="product-statement-small-img-and-text-text-left">
                 <div class="product-statement-small-img">
-                    <img class="img-responsive" src="wp-content/themes/temporall_site/assets/images/ic_lock_24px (1).svg">
+                    <img class="img-responsive" src="wp-content/themes/temporall_site/assets/images/bar_chart (1).svg">
                 </div>
                 <div class="product-statment-text">
-                    <p>Discover and unlock <br> previously untapped value
-                        <p>Combine data from Workbench and enterprise <br> productivity
-                            tools, CRM, and HR systems to discover and <br> unlock opportunities to deliver
-                            cross-systems insight; <br> build networks, improve productivity, drive engagement.
+                    <p class="heading3">Measure organisational impact <br> with our financial dashboard
+                        <p class="body-text">Turn key goals into organisational performance <br>
+                            indicators, utilise over fifty revenue and income <br> metrics pre-configured or
+                            create your own to meet <br> your specific needs.
                 </div>
             </div>
-        </div>
-
-
-        <div class="product-statement">
-            <div class="productStatement-wrapper">
-                <div class="product-statement-small-img-and-text-text-left">
-                    <div class="product-statement-small-img">
-                        <img class="img-responsive" src="wp-content/themes/temporall_site/assets/images/bar_chart (1).svg">
-                    </div>
-                    <div class="product-statment-text">
-                        <p>Measure organisational impact <br> with our financial dashboard
-                            <p>Turn key goals into organisational performance <br>
-                                indicators, utilise over fifty revenue and income <br> metrics pre-configured or
-                                create your own to meet <br> your specific needs.
-                    </div>
-                </div>
-                <div class="product-statement-main-img">
-                    <img src="wp-content/themes/temporall_site/assets/images/Temporall--Macbook-Screen-3.png">
-                </div>
+            <div class="product-statement-main-img">
+                <img src="wp-content/themes/temporall_site/assets/images/Temporall--Macbook-Screen-3.png">
             </div>
         </div>
+    </div>
 
-        <div class="product-statement-only-text">
-            <div class="container">
-                <p> Continuous organisational insights to help you
-                    deliver strategic transformation efforts such as digital
-                    transformation, mergers & acquisitions, and major change programmes. </p>
-            </div>
+    <div class="product-statement-only-text">
+        <div class="container">
+            <p class="heading3"> Continuous organisational insights to help you
+                deliver strategic transformation efforts such as digital
+                transformation, mergers & acquisitions, and major change programmes. </p>
         </div>
+    </div>
 
-        <?php include "wp-content/themes/temporall_site/static-components/html/footer.html" ?>
+    <?php get_footer(); ?>
 
-        <script src="./index.js"></script>
-        <script src="./static-components/js/include.js"></script>
-        <script  src="./static-components/js/top-nav.js"></script>
-        <script  src="./static-components/js/integration-card.js"></script>
-        <script>
-            includeHTML();
-        </script>
-         <script>topNav("top-nav");</script>
-         <script> left_text_right_image_card("devices-icon", "Mergers & Acquisition", "Are your cultures aligned?", "Mergers and acquisitions don’t always live up to expectations. Companies of all shapes and sizes actively engaged in M&A growth strategies must be mindful of how company culture plays a vital role in M&A success.<br><br> The two merging organisation may define culture differently, with different success criteria that can create frustration or anxiety as employees struggle to figure out what is the new “right” looks like across the new organisation.<br><br> Are you experiencing this?","new");  </script>
-         <script src="wp-content/themes/temporall_site/static-components/js/top-nav.js  "></script>
+    <script src="wp-content/themes/temporall_site/static-components/js/top-nav.js"></script>
+
 </body>
 
 </html>
