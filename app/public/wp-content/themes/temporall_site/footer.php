720c28633b758e114227a4f9d9a4f0383f2ce3c4
Bitcube <bitcube@Bitcubes-iMac.local>
2019-09-17 11:06:28 +0200

    added code to header and footer

---
+++ app/public/wp-content/themes/temporall_site/footer.php
@@ -0,0 +1,56 @@
+
+
+    <div class="footer-wrapper"> 
+        <div class="footer-image-text">
+            <div class="footer-imageText heading2">
+                discover • measure • transform
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="footer-flex-container">
+                <div class="footer-flex-item footer-margin-left">
+                    <div class="body-text">Platform</div>
+                    <div class="footer-text">
+                        <a href="#"> Workbench </a>
+                        <a href="#"> Integrations </a>
+                        <a href="#"> Pricing </a>
+                    </div>
+                </div>
+                <div class="footer-flex-item">
+                    <div class="body-text">Company</div>
+                    <div class="footer-text">
+                        <a href="#"> Board</a>
+                        <a href="#"> Team</a>
+                        <a href="#"> Blogs </a>
+                        <a href="#"> Culture Analytics </a>
+                        <a href="#"> Terms & Privacy </a>
+                    </div>
+
+                </div>
+                <div class="footer-flex-item">
+                    <div class="body-text">Contact</div>
+                    <div class="footer-text">
+                        <p> contactus@temporall.com </p>
+                        <p> +44 (0)20 7164 6830 </p>
+                        <a href="#"> Newsletter </a>
+                        <a href="#"> Careers </a>
+                    </div>
+                </div>
+                <div class="footer-flex-item footer-margin-right">
+                    <div class="body-text"><img src="wp-content/themes/temporall_site/assets/images/Path 62.svg"></div>
+                    <div class="footer-text">
+                        <p> © Copyright 2019 </p>
+                        <p> Temporall All Rights Reserved </p>
+                        <p> VOX Studios, 1-45 Durham Street, </p>
+                        <p> London, SE11 5JH </p>
+                    </div>
+                    <img style="padding-right: 15px;" src="wp-content/themes/temporall_site/assets/images/Twitter.svg">
+                    <img src="wp-content/themes/temporall_site/assets/images/Path 604.svg">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
