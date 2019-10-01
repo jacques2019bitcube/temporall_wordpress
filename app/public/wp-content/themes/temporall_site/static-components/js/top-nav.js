1567b5e90e5bf9f9b7b786fd6d30d2460901e1f2
Carmen Dominguez <carmen@bitcube.co.za>
2019-10-01 17:58:05 +0200

    Updated nav

--- app/public/wp-content/themes/temporall_site/static-components/js/top-nav.js
+++ app/public/wp-content/themes/temporall_site/static-components/js/top-nav.js
@@ -1,44 +1,44 @@
-$('#top-nav').ready(function () {
+$("#top-nav").ready(function() {
+  var showHide = true;
+  $(".mobile-menu-burger").on("click", function() {
+    if (!showHide) {
+      $(".mobile-menu").hide();
+    } else {
+      $(".mobile-menu").show();
+    }
+    showHide = !showHide;
+  });
 
-    var showHide = true;
-    $(".mobile-menu-burger").on('click', function () {
-        if (!showHide) {
-            $(".mobile-menu").hide();
-        }
-        else {
-            $(".mobile-menu").show();
-        }
-        showHide = !showHide;
-    })
+  // close menu if click outside it
+  $("body").on("click", function(e) {
+    var $target = $(e.target);
 
-    // close menu if click outside it
-    $('body').on('click', function (e) {
-        var $target = $(e.target);
+    if (
+      !$target.is(".mobile-menu-burger") &&
+      !$target.is(".mobile-menu-burger *")
+    ) {
+      $(".mobile-menu").hide();
+    }
 
-        if (!$target.is('.mobile-menu-burger') && !$target.is('.mobile-menu-burger *') ) {
-            $(".mobile-menu").hide();
-        }
+    if (
+      !$target.is(".menu-dropdown") &&
+      !$target.is(".menu-dropdown *") &&
+      !$target.is("#activate-sub-dropdown") &&
+      !$target.is("#activate-sub-dropdown *")
+    ) {
+      $(".dropdown").hide();
+      $(".sub-dropdown").hide();
+    }
+    // !$target.is('.menu-dropdown') && !$target.is('.menu-dropdown *')
+  });
 
-        if (!$target.is('.menu-dropdown') && !$target.is('.menu-dropdown *') && !$target.is('#activate-sub-dropdown') && !$target.is('#activate-sub-dropdown *') ) {
-            $(".dropdown").hide();
-            $(".sub-dropdown").hide();
-        }
-        // !$target.is('.menu-dropdown') && !$target.is('.menu-dropdown *')
+  $(".email").on("click", function() {});
 
-    });
-
-    $(".email").on('click', function () {
-
-    });
-
-    $(".phone").on('click', function () {
-
-    });
-    $(".menu-dropdown").on('click', function () {
-        $(".dropdown").show();
-    });
-    $("#activate-sub-dropdown").on('click', function () {
-        $(".sub-dropdown").show();
-    });
-
+  $(".phone").on("click", function() {});
+  $(".menu-dropdown").on("click", function() {
+    $(".dropdown").show();
+  });
+  $("#activate-sub-dropdown").on("click", function() {
+    $(".sub-dropdown").show();
+  });
 });
