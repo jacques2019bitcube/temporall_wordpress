34cfd416fdc19ed066d1698df9bec8f2d8799b20
Carmen Dominguez <carmen@bitcube.co.za>
2019-10-01 17:57:24 +0200

    Updated footer

--- app/public/wp-content/themes/temporall_site/footer.php
+++ app/public/wp-content/themes/temporall_site/footer.php
@@ -1,4 +1,4 @@
-<div class="footer-wrapper">
+<div class="footer-wrapper" style="background: #f8f8f8 0% 0% no-repeat padding-box">
     <!-- <div class="footer-image-text">
     </div> -->
     <div class="footer">
@@ -10,7 +10,6 @@
                     <a href="/board"> Executive team</a>
                     <!-- <a href="#"> Blogs </a> -->
                     <!-- <a href="/press"> Press</a> -->
-                    <a href="/privacy-policy"> Terms & Privacy </a>
                 </div>
             </div>
             <div class="footer-flex-item">
@@ -19,6 +18,7 @@
                     <a href="mailto:contactus@temporall.com">contactus@temporall.com </a>
                     <a href="tel:+44 (0)20 7164 6830">+44 (0)20 7164 6830 </a>
                     <a href="/newsletter"> Newsletter </a>
+                    <a href="/privacy-policy"> Terms & Privacy </a>
                     <!-- <a href="/careers"> Careers </a> -->
                 </div>
             </div>
@@ -30,8 +30,10 @@
                     <p> VOX Studios, 1-45 Durham Street, </p>
                     <p> London, SE11 5JH </p>
                 </div>
+                <div style="display: flex">
                 <a target="_blank" href="https://twitter.com/temporalluk?lang=en"><img style="padding-right: 15px;" src="/wp-content/themes/temporall_site/assets/images/Twitter.svg"></a>
                 <a target="_blank" href="https://www.linkedin.com/company/temporall/"><img src="/wp-content/themes/temporall_site/assets/images/Path 604.svg"></a>
+                </div>
             </div>
         </div>
     </div>
